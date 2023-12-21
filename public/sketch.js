ab5f514ea30954e42ae55010458c285374f56566
honk7608 <honk7608@gmail.com>
2023-12-22 01:14:46 +0900

    Mask 기능 조정
    위치 조정(1)
    선택 기능 보완 등

--- public/sketch.js
+++ public/sketch.js
@@ -4,32 +4,35 @@ let canvasHeight = 480
 
 let images
 let s
-let maskL
-let maskR
+let testmask
+let mask1L
+let mask1R
+let mask2
+let mask3
+let mask4
 
-let selectInput
+let setSelectedOption = 0
+let currentSelectedOption = 0
 
 function setup() {
-  let img2 = loadImage('/assets/ak1.png');
-  let img3 = loadImage('/assets/an.png');
-  let img4 = loadImage('/assets/bok.png');
-  let img5 = loadImage('/assets/bul.png');
+  testmask = loadImage('/assets/testPoint.png');
+  mask1R = loadImage('/assets/ak1.png');
+  mask1L = loadImage('/assets/ak2.png');
+  mask2 = loadImage('/assets/an.png');
+  mask3 = loadImage('/assets/bok.png');
+  mask4 = loadImage('/assets/bul.png');
   let img6 = loadImage('/assets/chun.png');
   let img7 = loadImage('/assets/cot.png');
   let img8 = loadImage('/assets/ha.png');
   let img9 = loadImage('/assets/ho.png');
   let img10 = loadImage('/assets/joong.png');
   let img11 = loadImage('/assets/pe.png');
-  let img12 = loadImage('/assets/ak2.png');
-  images = [img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12]; 
-  maskR = img2;
-  maskL = img12;
 
   //canvas ÀÌµ¿
   createCanvas(canvasWidth, 360)
   canvasElement = document.getElementsByTagName("canvas")[0]
   document.getElementsByTagName("main")[0].appendChild(canvasElement)
-  
+
   // setup the webcam capture
   capture = createCapture(VIDEO)
   capture.hide()
@@ -53,8 +56,37 @@ function draw() {
   // get the face positions from the tracker
   let positions = tracker.getCurrentPosition()  
 
-  if(positions.length > 0) {
-    image(maskR, positions[15][0]-10, positions[15][1]-110, positions[11][0]-positions[3][0]+20, positions[11][0]-positions[3][0]+20);
-    image(maskL, positions[19][0]-100, positions[19][1]-110, positions[11][0]-positions[3][0]+20, positions[11][0]-positions[3][0]+20);
+  selectedElement = document.querySelector('input[type="radio"]:checked')
+  
+  if(selectedElement) {currentSelectedOption = Number(selectedElement.value)}
+  else{currentSelectedOption = 0}
+
+  if(currentSelectedOption != setSelectedOption) {setSelectedOption = currentSelectedOption}
+
+  if(positions.length > 0) { //ak
+    if(setSelectedOption == 1) {
+      image(mask1R, positions[15][0]-30, positions[15][1]-100, positions[11][0]-positions[3][0], positions[7][1]-positions[33][1]);
+      image(mask1L, positions[19][0]-90, positions[19][1]-100, positions[11][0]-positions[3][0], positions[7][1]-positions[33][1]);
+    } else if (setSelectedOption == 2) { //an
+      dx = (positions[32][0]-positions[27][0])*3
+      dy = (positions[41][1]-positions[33][1])*2.5
+      image(mask2, (positions[0][0]+positions[14][0]-dx)/2, (positions[33][1]+positions[41][1]-dy+10)/2, dx, dy)
+    } else if (setSelectedOption == 3) { //bok
+      dx = (positions[13][0]-positions[1][0])*1.5
+      dy = (positions[7][1]-positions[33][1])*1.5
+      image(mask3, (positions[62][0]-dx/2), (positions[62][1]-dy/2+20), dx, dy)
+    } else if (setSelectedOption == 4) { //bul
+      dx = (positions[13][0]-positions[1][0])*2
+      dy = (positions[7][1]-positions[62][1])*3.5
+      image(mask4, (positions[62][0]-dx/2), (positions[62][1]-dy/2-100), dx, dy)
+    } 
+    else {
+      i = 0
+      while (i<=70) {
+        image(testmask, positions[i][0], positions[i][1], 5, 5)
+        i += 1
+      }
+    }
+    
   }
 }
--- src/app/function/Mask/page.js
+++ src/app/function/Mask/page.js
@@ -1,53 +1,65 @@
+'use client'
 import './mask.css'
+
 export default function Func_page(){
     return(
         <>
+            <script src="/sketch.js"></script>
             <div className='option'>
-                <span className='div_title'>세부 설정 및 기능</span>
+                <span className='div_title'>기능 선택</span>
                 <div className="selector">
-                    <input type="radio" id='mask1' value={1}   name="maskType"/>1
-                    <label htmlFor='mask1' >
-                    </label>
+                    <div className='option'>
+                        <input type="radio" id='mask1' value={1}   name="maskType"/>
+                        <label id='mask1Label' htmlFor='mask1' >악마 뿔</label>
+                    </div>
+
+                    <div className='option'>
+                        <input type="radio" id='mask2' value={2}   name="maskType"/>
+                        <label id='mask2Label' htmlFor='mask2' >안경</label>
+                    </div>
+                        
+                    <div className='option'>
+                        <input type="radio" id='mask3' value={3}   name="maskType"/>
+                        <label id='mask3Label' htmlFor='mask3' >복면</label>
+                    </div>
+
+                    <div className='option'>                    
+                        <input type="radio" id='mask4' value={4}   name="maskType"/>
+                        <label id='mask4Label' htmlFor='mask4' >불타는 얼굴</label>
+                    </div>
+
+                    <div className='option'>
+                        <input type="radio" id='mask5' value={5}   name="maskType"/>
+                        <label id='mask5Label' htmlFor='mask5' >천사 링</label>
+                    </div>
                     
-                    <input type="radio" id='mask2' value={2}   name="maskType"/>2
-                    <label htmlFor='mask2' >
-                    </label>
-                    
-                    <input type="radio" id='mask3' value={3}   name="maskType"/>3
-                    <label htmlFor='mask3' >
-                    </label>
-                    
-                    <input type="radio" id='mask4' value={4}   name="maskType"/>4
-                    <label htmlFor='mask4' >
-                    </label>
-                    
-                    <input type="radio" id='mask5' value={5}   name="maskType"/>5
-                    <label htmlFor='mask5' >
-                    </label>
-                    
-                    <input type="radio" id='mask6' value={6}   name="maskType"/>6
-                    <label htmlFor='mask6' >
-                    </label>
-                    
-                    <input type="radio" id='mask7' value={7}   name="maskType"/>7
-                    <label htmlFor='mask7' >
-                    </label>
-                    
-                    <input type="radio" id='mask8' value={8}   name="maskType"/>8
-                    <label htmlFor='mask8' >
-                    </label>
-                    
-                    <input type="radio" id='mask9' value={9}   name="maskType"/>9
-                    <label htmlFor='mask9' >
-                    </label>
-                    
-                    <input type="radio" id='mask10' value={10} name="maskType"/>0
-                    <label htmlFor='mask10'>
-                    </label>
+                    <div className='option'>
+                        <input type="radio" id='mask6' value={6}   name="maskType"/>
+                        <label id='mask6Label' htmlFor='mask6' >콧수염</label>
+                    </div>
+                        
+                    <div className='option'>
+                        <input type="radio" id='mask7' value={7}   name="maskType"/>
+                        <label id='mask7Label' htmlFor='mask7' >해골</label>
+                    </div>
+                        
+                    <div className='option'>
+                        <input type="radio" id='mask8' value={8}   name="maskType"/>
+                        <label id='mask8Label' htmlFor='mask8' >호박</label>
+                    </div>
+                        
+                    <div className='option'>
+                        <input type="radio" id='mask9' value={9}   name="maskType"/>
+                        <label id='mask9Label' htmlFor='mask9' >중세 투구</label>
+                    </div>
+                        
+                    <div className='option'>
+                        <input type="radio" id='mask10' value={10} name="maskType"/>
+                        <label id='mask10Label' htmlFor='mask10' >페페</label>
+                    </div>
                 </div>
             </div>
             <main>
-                <script src="/sketch.js"></script>
             </main>
         </>
         )
