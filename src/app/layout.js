9191bcc73a2dce41545bcc0e87e0711052cf481e
honk7608 <honk7608@gmail.com>
2023-12-26 00:10:30 +0900

    Fix; heroku deploy fail

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,9 @@ export default function RootLayout({ children }) {
     return (
         <html>
             <head>
-                <Script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/p5.js"/>
-                <Script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/addons/p5.sound.min.js"/>
-                <Script src="https://www.auduno.com/clmtrackr/build/clmtrackr.min.js"/>
+                <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/p5.js"/>
+                <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/addons/p5.sound.min.js"/>
+                <Script strategy="beforeInteractive" src="https://www.auduno.com/clmtrackr/build/clmtrackr.min.js"/>
 
                 <link rel="stylesheet" as="style" crossOrigin="true" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"/>
                 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700;900&display=swap" rel="stylesheet"></link>
